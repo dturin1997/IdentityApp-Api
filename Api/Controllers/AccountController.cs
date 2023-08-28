2685bcd18be7077e7fc3d159dd19fff745685d04
Dario Turin <dturin1997@gmail.com>
2023-08-27 19:27:59 -0500

    Video-09-udemy: Account controller and login

---
+++ Api/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using Api.DTOs.Account;
+using Api.Models;
+using Api.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly JWTService _jwtService;
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+
+        public AccountController(JWTService jwtService,
+            SignInManager<User> signInManager,
+            UserManager<User> userManager)
+        {
+            _jwtService = jwtService;
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto model)
+        {
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user == null) return Unauthorized("Invalid username or password");
+
+            if (user.EmailConfirmed == false) return Unauthorized("Please confirm you email.");
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+            if (!result.Succeeded) return Unauthorized("Invalid username or password");
+
+            return CreateApplicationUserDto(user);
+        }      
+
+        #region Private Helper Methods
+        private UserDto CreateApplicationUserDto(User user)
+        {
+            return new UserDto
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                JWT = _jwtService.CreateJWT(user),
+            };
+        }
+        #endregion
+    }
+}
