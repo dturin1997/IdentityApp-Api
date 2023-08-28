7eefbbbe44db7f59807f5106731352b76574d731
Dario Turin <dturin1997@gmail.com>
2023-08-27 19:37:02 -0500

    Video-10-udemy: Register user

--- Api/Controllers/AccountController.cs
+++ Api/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -39,7 +40,31 @@ namespace Api.Controllers
             if (!result.Succeeded) return Unauthorized("Invalid username or password");
 
             return CreateApplicationUserDto(user);
-        }      
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto model)
+        {
+            if (await CheckEmailExistsAsync(model.Email))
+            {
+                return BadRequest($"An existing account is using {model.Email}, email addres. Please try with another email address");
+            }
+
+            var userToAdd = new User
+            {
+                FirstName = model.FirstName.ToLower(),
+                LastName = model.LastName.ToLower(),
+                UserName = model.Email.ToLower(),
+                Email = model.Email.ToLower(),
+                EmailConfirmed = true
+            };
+
+            // creates a user inside our AspNetUsers table inside our database
+            var result = await _userManager.CreateAsync(userToAdd, model.Password);
+            if (!result.Succeeded) return BadRequest(result.Errors);
+
+            return Ok("Your account has been created, you can login");
+        }
 
         #region Private Helper Methods
         private UserDto CreateApplicationUserDto(User user)
@@ -50,6 +75,11 @@ namespace Api.Controllers
                 LastName = user.LastName,
                 JWT = _jwtService.CreateJWT(user),
             };
+        }
+
+        private async Task<bool> CheckEmailExistsAsync(string email)
+        {
+            return await _userManager.Users.AnyAsync(x => x.Email == email.ToLower());
         }
         #endregion
     }
