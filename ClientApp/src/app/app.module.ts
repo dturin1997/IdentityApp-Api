c761c7c612d3e07ae73159f7a1c2bd429ad9775c
Dario Turin <dturin1997@gmail.com>
2023-08-31 22:04:49 -0500

    Video-17-udemy: Design Navbar and Footer

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -7,20 +7,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { SharedModule } from './shared/shared.module';
+import { PlayComponent } from './play/play.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    FooterComponent,
-    HomeComponent
-  ],
+  declarations: [AppComponent, NavbarComponent, FooterComponent, HomeComponent, PlayComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    SharedModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
