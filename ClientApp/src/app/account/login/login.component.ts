f6e50e629e2fa2a4882212f1b2774087bf5ae140
Dario Turin <dturin1997@gmail.com>
2023-10-31 18:30:40 -0500

    Video-27-udemy: Persisting user part 3

--- ClientApp/src/app/account/login/login.component.ts
+++ ClientApp/src/app/account/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AccountService } from '../account.service';
+import { User } from 'src/app/shared/models/user';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -12,12 +14,21 @@ export class LoginComponent implements OnInit {
   loginForm: FormGroup = new FormGroup({});
   submitted = false;
   errorMessages: string[] = [];
+  returnUrl: string | null = null;
 
   constructor(
     private accountService: AccountService,
     private formBuilder: FormBuilder,
     private router: Router
-  ) {}
+  ) {
+    this.accountService.user$.pipe(take(1)).subscribe({
+      next: (user: User | null) => {
+        if (user) {
+          this.router.navigateByUrl('/');
+        }
+      },
+    });
+  }
 
   ngOnInit(): void {
     this.initializeForm();
@@ -37,7 +48,11 @@ export class LoginComponent implements OnInit {
     if (this.loginForm.valid) {
       this.accountService.login(this.loginForm.value).subscribe({
         next: (response: any) => {
-          //console.log(response);
+          if (this.returnUrl) {
+            this.router.navigateByUrl(this.returnUrl);
+          } else {
+            this.router.navigateByUrl('/');
+          }
         },
         error: (error) => {
           if (error.error.errors) {
