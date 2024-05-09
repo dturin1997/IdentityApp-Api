568214c7ebf572bdfd868e8b6cfe80fc541a6ca2
Dario Turin <dturin1997@gmail.com>
2024-05-09 02:45:17 -0500

    Video-31-udemy: Alternative way of sending email Using SMTP

--- Api/Services/EmailService.cs
+++ Api/Services/EmailService.cs
@@ -1,8 +1,14 @@
 ﻿using Api.DTOs.Account;
 using Mailjet.Client;
+using Mailjet.Client.Resources;
 using Mailjet.Client.TransactionalEmails;
 using Microsoft.Extensions.Configuration;
+using System.Net.Mail;
+using System.Net;
+using System;
 using System.Threading.Tasks;
+using Microsoft.Identity.Client.Platforms.Features.DesktopOs.Kerberos;
+using System.Net.Http;
 
 namespace Api.Services
 {
@@ -15,6 +21,7 @@ namespace Api.Services
             _config = config;
         }
 
+        /*
         public async Task<bool> SendEmailAsync(EmailSendDto emailSend)
         {
             MailjetClient client = new MailjetClient(_config["MailJet:ApiKey"], _config["MailJet:SecretKey"]);
@@ -37,5 +44,34 @@ namespace Api.Services
 
             return false;
         }
+        */
+
+        public async Task<bool> SendEmailAsync(EmailSendDto emailSend)
+        {
+            try
+            {
+                var username = _config["SMTP:Username"];
+                var password = _config["SMTP:Password"];
+                var client = new SmtpClient("smtp-mail.outlook.com", 587)
+                {
+
+                    EnableSsl = true,
+                    Credentials = new NetworkCredential(username, password)
+                };
+
+                var message = new MailMessage(from: username, to: emailSend.To, subject: emailSend.Subject, body: emailSend.Body);
+
+                message.IsBodyHtml = true;
+                await client.SendMailAsync(message);
+                return true;
+            }
+            catch (Exception ex) {
+
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+
+        }
     }
+
 }
